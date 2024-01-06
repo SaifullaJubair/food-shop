12e6e19621460c2b50a47a764798d389330911e0
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-06 14:54:23 +0600

    tailwind install

--- client-site/src/App.js
+++ client-site/src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">test</h1>
+    </div>
+  );
 }
 
 export default App;
