565c0f7aef7331ff624772c727b22bce7fad2e50
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-07 14:19:13 +0600

    setup route

--- client-site/src/App.js
+++ client-site/src/App.js
@@ -1,8 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Routes/Routes";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
       <h1 className="text-3xl">test</h1>
       <button className="btn-primary">Button</button>
     </div>
