c39b16c17fd0b625ca3ca7ee0852646206f63963
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-07 12:20:02 +0600

    Layout create

--- client-site/src/App.js
+++ client-site/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl">test</h1>
+      <button className="btn-primary">Button</button>
     </div>
   );
 }
